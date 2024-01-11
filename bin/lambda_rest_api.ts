28be7c9a8d9f58de14a476291316ea0978daeb85
Magne Garaas <magne.garaas@oslobystyre.no>
2024-01-11 19:22:04 +0100

    first commit

--- bin/lambda_rest_api.ts
+++ bin/lambda_rest_api.ts
@@ -1,21 +1,49 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { LambdaRestApiStack } from '../lib/lambda_rest_api-stack';
+
+import { IConfig } from '../interfaces/config.interface';
+import * as config from '../config.json';
+import { DBStack } from '../lib/db.stack';
+import { UserpoolStack } from '../lib/userpool.stack';
+import { APIStack } from '../lib/api.stack';
+import { LambdaStack } from '../lib/lambda.stack';
+const myConfig: IConfig = config as IConfig;
+
+const ACCOUNT  = myConfig.AWS.ACCOUNT_ID ??  process.env.CDK_DEFAULT_ACCOUNT;
+const REGION = myConfig.AWS.REGION ?? process.env.CDK_DEFAULT_REGION;
 
 const app = new cdk.App();
-new LambdaRestApiStack(app, 'LambdaRestApiStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const dbStack = new DBStack(app, myConfig.APP.name + 'DBStack', {
+  appName: myConfig.APP.name,
+  env: { account: ACCOUNT, region: REGION },
+  db: {
+    ENV: myConfig.DB.ENV,
+    usertable: {
+      tableName: myConfig.DB.tables.USER
+    }
+  }
+})
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const lambdaStack = new LambdaStack(app, myConfig.APP.name + 'LambdaStack', {
+  appName: myConfig.APP.name,
+  env: { account: ACCOUNT, region: REGION },
+  dbs: {
+    user: dbStack.userTable
+  }
+});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const userpoolStack = new UserpoolStack(app, myConfig.APP.name + 'UserpoolStack', {
+  env: { account: ACCOUNT, region: REGION },
+  appName: myConfig.APP.name
+})
+
+new APIStack(app, myConfig.APP.name + 'APIStack', {
+  env: { account:ACCOUNT, region: REGION},
+  appName: myConfig.APP.name,
+  lambdas: {
+    getUserLambda: lambdaStack.getUser
+  },
+  userpool : userpoolStack.pool
 });
